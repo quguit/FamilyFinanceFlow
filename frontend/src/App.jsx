ab2bd5492493cb02229f6176ecc1bb92d30cdcb7
Guilherme <engguilhermen@gmail.com>
2025-07-13 17:02:53 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import Login from "../pages/Login"
+import Dashboard from "../pages/Dashboard";
 
 
 function App() {
